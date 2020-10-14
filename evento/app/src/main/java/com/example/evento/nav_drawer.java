90e8532567a8ca45b672e3ef979590dd174916ad
Sandhya <sandhyasankar29@gmail.com>
2020-10-14 13:06:12 +0530

    event database added

---
+++ evento/app/src/main/java/com/example/evento/nav_drawer.java
@@ -0,0 +1,13 @@
+package com.example.evento;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class nav_drawer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nav_drawer);
+    }
+}
